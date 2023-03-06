4bae007c59fc408c22c5ba81a24966516e8905c0
jack mcqueen <jdmcqueen44@gmail.com>
2023-03-06 17:06:49 +0000

    Load media assets, add initial responsiveness

--- src/home/HomeLinks.js
+++ src/home/HomeLinks.js
@@ -1,18 +1,27 @@
+import audio from "../assets/media/AUDIO.gif";
+import visual from "../assets/media/VISUAL.gif";
+
 export default function HomeLinks() {
     return (
         <nav>
             <ul className="external">
                 <li>
                     <a href="https://soundcloud.com/goodluckwhiterabbit" target="_blank" rel="noreferrer">
-                        AUDIO
+                        <img className="shimmer" src={audio} alt="The word audio shimmering"/>
                     </a>
                 </li>
 
                 <li>
-                    <a href="">VISUAL</a>
+                    <a href="">
+                        <img className="shimmer" src={visual} alt="The word visual shimmering"/>
+                    </a>
 
                     <div className="subnav">
                         <ul className="subnav-links">
+                            <li>
+                                |
+                            </li>
+
                             <li>
                                 <a href="">ART</a>
                             </li>
